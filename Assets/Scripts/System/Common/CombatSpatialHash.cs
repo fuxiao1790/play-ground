d026bfd4b70c51bf9acc165375a65e5401b221c8
user name <xiao.fu1790@gmail.com>
2026-07-04 20:51:10 -0400

    benchmark config

--- Assets/Scripts/System/Common/CombatSpatialHash.cs
+++ Assets/Scripts/System/Common/CombatSpatialHash.cs
@@ -6,7 +6,7 @@ namespace PlayGround.System.Common
     {
         internal const float ProjectileCollisionCellSize = 1f;
         internal const float TrackingCellSize = 64f;
-        internal const float AoeCellSize = 64f;
+        internal const float AoeCellSize = 32f;
 
         internal static int2 FloorCell(float2 pos, float cellSize) =>
             new(
