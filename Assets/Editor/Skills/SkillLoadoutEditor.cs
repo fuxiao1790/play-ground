c023761d37fb84bb6653a37fab880959cfa6bc55
user name <xiao.fu1790@gmail.com>
2026-07-11 08:06:04 -0400

    qol

--- Assets/Editor/Skills/SkillLoadoutEditor.cs
+++ Assets/Editor/Skills/SkillLoadoutEditor.cs
@@ -1,5 +1,6 @@
 using PlayGround.Skills;
 using UnityEditor;
+using UnityEditorInternal;
 using UnityEngine;
 
 namespace PlayGround.Editor.Skills
@@ -9,11 +10,20 @@ namespace PlayGround.Editor.Skills
     {
         private SerializedProperty _slots;
         private SerializedProperty _maxRootSets;
+        private ReorderableList _list;
+        private int _removeAt = -1;
 
         private void OnEnable()
         {
             _slots = serializedObject.FindProperty("slots");
             _maxRootSets = serializedObject.FindProperty("maxRootSets");
+
+            _list = new ReorderableList(serializedObject, _slots, true, true, false, false)
+            {
+                drawHeaderCallback = rect => EditorGUI.LabelField(rect, "Slots"),
+                elementHeightCallback = _ => EditorGUIUtility.singleLineHeight + 4,
+                drawElementCallback = DrawElement,
+            };
         }
 
         public override void OnInspectorGUI()
@@ -22,9 +32,12 @@ namespace PlayGround.Editor.Skills
 
             EditorGUILayout.PropertyField(_maxRootSets);
             EditorGUILayout.Space(6);
-            EditorGUILayout.LabelField("Slots", EditorStyles.boldLabel);
 
-            DrawSlotList();
+            _removeAt = -1;
+            _list.DoLayoutList();
+            if (_removeAt >= 0)
+                _slots.DeleteArrayElementAtIndex(_removeAt);
+
             DrawDropZone();
 
             EditorGUILayout.Space(2);
@@ -38,42 +51,38 @@ namespace PlayGround.Editor.Skills
             serializedObject.ApplyModifiedProperties();
         }
 
-        private void DrawSlotList()
+        private void DrawElement(Rect rect, int index, bool isActive, bool isFocused)
         {
-            int removeAt = -1;
+            SerializedProperty slot = _slots.GetArrayElementAtIndex(index);
+            string typeName = slot.managedReferenceFullTypename;
 
-            for (int i = 0; i < _slots.arraySize; i++)
+            rect.y += 2;
+            rect.height = EditorGUIUtility.singleLineHeight;
+
+            var labelRect = new Rect(rect.x, rect.y, 46, rect.height);
+            var fieldRect = new Rect(rect.x + 50, rect.y, rect.width - 50 - 24, rect.height);
+            var removeRect = new Rect(rect.xMax - 20, rect.y, 20, rect.height);
+
+            if (typeName.Contains(nameof(SkillSetSlot)))
             {
-                SerializedProperty slot = _slots.GetArrayElementAtIndex(i);
-                string typeName = slot.managedReferenceFullTypename;
-
-                EditorGUILayout.BeginHorizontal();
-
-                if (typeName.Contains(nameof(SkillSetSlot)))
-                {
-                    SerializedProperty skillSetProp = slot.FindPropertyRelative("skillSet");
-                    EditorGUILayout.LabelField("Skill", GUILayout.Width(46));
-                    EditorGUILayout.ObjectField(skillSetProp, typeof(SkillSet), GUIContent.none);
-                }
-                else if (typeName.Contains(nameof(TriggerLinkSlot)))
-                {
-                    SerializedProperty linkProp = slot.FindPropertyRelative("link");
-                    EditorGUILayout.LabelField("Trigger", GUILayout.Width(46));
-                    EditorGUILayout.ObjectField(linkProp, typeof(TriggerLink), GUIContent.none);
-                }
-                else
-                {
-                    EditorGUILayout.LabelField("(null slot)");
-                }
-
-                if (GUILayout.Button("-", GUILayout.Width(20)))
-                    removeAt = i;
-
-                EditorGUILayout.EndHorizontal();
+                SerializedProperty skillSetProp = slot.FindPropertyRelative("skillSet");
+                EditorGUI.LabelField(labelRect, "Skill");
+                EditorGUI.ObjectField(fieldRect, skillSetProp, typeof(SkillSet), GUIContent.none);
+            }
+            else if (typeName.Contains(nameof(TriggerLinkSlot)))
+            {
+                SerializedProperty linkProp = slot.FindPropertyRelative("link");
+                EditorGUI.LabelField(labelRect, "Trigger");
+                EditorGUI.ObjectField(fieldRect, linkProp, typeof(TriggerLink), GUIContent.none);
+            }
+            else
+            {
+                EditorGUI.LabelField(rect, "(null slot)");
+                return;
             }
 
-            if (removeAt >= 0)
-                _slots.DeleteArrayElementAtIndex(removeAt);
+            if (GUI.Button(removeRect, "-"))
+                _removeAt = index;
         }
 
         private void DrawDropZone()
--- Docs/todo.md
+++ Docs/todo.md
@@ -4,9 +4,7 @@ minor performance related refacor
 - collision system use aabb tree instead of spatial hash. aabb tree is more simd friendly.
 
 new feature
-- mob spawn rework
 - cast on crit and other conditions.
 - fast projectiles, these cannot have tracking, requires tunneling check
 - unit related, these have dependency on each other 
-    - mob skills (mob should also use the same skill loadout for player, player skill loadout should be generic)
     - player summons (these should just be mobs but with different faction)
